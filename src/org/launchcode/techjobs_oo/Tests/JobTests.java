54d348109a9d02e0c527098edc1fbbb98ea85242
Alex Levy <alex.j.levy@gmail.com>
2019-12-06 10:40:22 -0600

    Step 4 complete

--- src/org/launchcode/techjobs_oo/Tests/JobTests.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTests.java
@@ -15,23 +15,35 @@ public class JobTests {
 Job testJob;
 Job otherTestJob;
 Job fancyTestJob;
+Job almostSameJob;
 
 @Before
 public void createTestJob(){
     testJob = new Job();
     otherTestJob = new Job();
     fancyTestJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType ("Quality Control"), new CoreCompetency("Persistence"));
+    almostSameJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType ("Quality Control"), new CoreCompetency("Persistence"));
 }
 @Test
 public void testSettingJobId(){
-    assertNotEquals(testJob, otherTestJob);
+    assertNotEquals(testJob.getId(), otherTestJob.getId());
+    assertTrue(testJob.getId()+1 == otherTestJob.getId() || testJob.getId()-1 == otherTestJob.getId());
 }
 @Test
 public void testJobContructorSetsAllFields(){
     assertEquals("Product tester", fancyTestJob.getName());
     assertEquals("ACME", fancyTestJob.getEmployer().getValue());
+    assertEquals("Desert", fancyTestJob.getLocation().getValue());
+    assertEquals("Quality Control", fancyTestJob.getPositionType().getValue());
+    assertEquals("Persistence", fancyTestJob.getCoreCompetency().getValue());
     assertTrue(fancyTestJob.getEmployer() instanceof Employer);
+    assertTrue(fancyTestJob.getLocation() instanceof Location);
+    assertTrue(fancyTestJob.getPositionType() instanceof PositionType);
+    assertTrue(fancyTestJob.getCoreCompetency() instanceof CoreCompetency);
 }
-
+@Test
+public void testJobsForEquality(){
+    assertFalse(fancyTestJob.equals(almostSameJob));
+}
 
 }
